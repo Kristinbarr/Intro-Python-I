e69f9aef1369e0883bd6a1f61aaa1db754352fc0
Kristin Barr <kristinbarr@gmail.com>
2020-01-14 16:57:51 -0500

    04 printing complete

--- src/04_printing.py
+++ src/04_printing.py
@@ -1,6 +1,6 @@
 """
 Python provides a number of ways to perform printing. Research
-how to print using the printf operator, the `format` string 
+how to print using the printf operator, the `format` string
 method, and by using f-strings.
 """
 
@@ -11,7 +11,14 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
+print('x is %d, y is %.2f, z is %s' % (x, y, z))
+# KB NOTES:
+# d is for decimal, f is for floating point decimal, s is for string
+# numbers between % and f are placeholder counts
+# 342.34 => 6.2 (6 total, 2 after decimal, decimal counts)
 
 # Use the 'format' string method to print the same thing
+print('x is {}, y is {:.2f}, z is {}'.format(x,y,z))
 
 # Finally, print the same thing using an f-string
+print(f'x is {x}, y is {y:.2f}, z is {z}')
